1af6db6392eedf59ebc63b0b700b9e6c91c195ca
Diego Leonardo <diegolf125@gmail.com>
2022-05-04 18:01:26 -0300

    Prisma setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+   return res.status(200).send();
+});
+
 app.listen(3333, () => {
    console.log("HTTP Server Running.");
 });
