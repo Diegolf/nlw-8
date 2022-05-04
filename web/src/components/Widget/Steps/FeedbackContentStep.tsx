fc6149c4739749bb9a1b052fb7037fda927c6bda
Diego Leonardo <diegolf125@gmail.com>
2022-05-04 09:39:40 -0300

    Feedback steps

--- web/src/components/Widget/Steps/FeedbackContentStep.tsx
+++ web/src/components/Widget/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,34 @@
-export function FeedbackContentStep() {
+import { ArrowLeft } from "phosphor-react";
+import { CloseButton } from "../CloseButton/CloseButton";
+import { FeedbackType, feedbackTypes } from "../WidgetForm/WidgetForm";
+
+interface FeedbackContentStepProps {
+   feedbackType: FeedbackType;
+   onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
    return (
-      <div></div>
+      <>
+         <header>
+            <button 
+               type="button" 
+               className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+               onClick={onFeedbackRestartRequested}
+            >
+               <ArrowLeft weight="bold" className="w-4 h-4" />
+            </button>
+            <span className="text-xl leading-6 flex items-center gap-2">
+               <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+               {feedbackTypeInfo.title}
+            </span>
+            <CloseButton />
+         </header>
+         <div className="flex py-8 gap-2 w-full">
+
+         </div>
+      </>
    );
 }
--- web/src/components/Widget/Steps/FeedbackSuccesssStep.tsx
+++ web/src/components/Widget/Steps/FeedbackSuccesssStep.tsx
@@ -1,5 +1,16 @@
+import { Fragment } from "react";
+import { CloseButton } from "../CloseButton/CloseButton";
+
 export function FeedbackSuccesssStep() {
    return (
-      <div></div>
+      <Fragment>
+         <header>
+            <span className="text-xl leading-6">Deixe seu feedback</span>
+            <CloseButton />
+         </header>
+         <div className="flex py-8 gap-2 w-full">
+           
+         </div>
+      </Fragment>
    );
 }
