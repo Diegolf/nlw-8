c8dae1c67cacb22c1b345cc1e51403a8acd4d863
Diego Leonardo <diegolf125@gmail.com>
2022-05-04 11:45:38 -0300

    Form submit

--- web/src/components/Widget/Steps/FeedbackContentStep.tsx
+++ web/src/components/Widget/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { ArrowLeft } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { CloseButton } from "../CloseButton/CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton/ScreenshotButton";
 import { FeedbackType, feedbackTypes } from "../WidgetForm/WidgetForm";
@@ -12,6 +12,12 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
    const feedbackTypeInfo = feedbackTypes[feedbackType];
    const [screenshot, setScreenshot] = useState<string | null>(null);
+   const [comment, setComment] = useState("");
+
+   function handleSubmitFeedback(event: FormEvent) {
+      event.preventDefault();
+      console.log({screenshot, comment});
+   }
 
    return (
       <>
@@ -30,10 +36,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
             <CloseButton />
          </header>
 
-         <form className="my-4 w-full">
+         <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
             <textarea
                className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus-brand resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                placeholder="Conte com detalhes o que está acontecendo..."
+               onChange={event => setComment(event.target.value)}
             />
 
             <footer className="flex gap-2 mt-2">
